bf002972966d28ae9bceb8af4f95d37a046a56d3
Yug Bothra <yugbothra200@outlook.com>
2025-11-27 22:26:15 +0530

    update backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,11 @@
+// src/App.jsx
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./components/Auth/AuthContext";
 import ProtectedRoute from "./components/Auth/ProtectedRoute";
+
+// GLOBAL DATA PROVIDER
+import { AppDataProvider } from "./context/AppDataContext";
 
 // Pages
 import LandingPage from "./components/Landing/LandingPage";
@@ -13,53 +18,55 @@ import SignUp from "./components/Auth/SignUp";
 
 // Resources
 import Blog from "./components/Resources/Blog/Blog";
-import ResumeBuilder from "./components/Resources/Blog/ResumeBuilder";   // ✅ Added
+import ResumeBuilder from "./components/Resources/Blog/ResumeBuilder";
 
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Routes>
+      <AppDataProvider> {/* 🔥 LOAD ALL SETTINGS + PERSONAS GLOBALLY */}
+        <BrowserRouter>
+          <Routes>
 
-          {/* Public Routes */}
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
+            {/* Public Routes */}
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
 
-          <Route path="/blog" element={<Blog />} />                    {/* ✅ Blog */}
-          <Route path="/resume-builder" element={<ResumeBuilder />} /> {/* ✅ Resume Builder */}
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/resume-builder" element={<ResumeBuilder />} />
 
-          {/* Protected Routes */}
-          <Route
-            path="/interview"
-            element={
-              <ProtectedRoute>
-                <Interview />
-              </ProtectedRoute>
-            }
-          />
+            {/* Protected Routes */}
+            <Route
+              path="/interview"
+              element={
+                <ProtectedRoute>
+                  <Interview />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path="/interview-assist"
-            element={
-              <ProtectedRoute>
-                <InterviewAssist />
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path="/interview-assist"
+              element={
+                <ProtectedRoute>
+                  <InterviewAssist />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path="/personas"
-            element={
-              <ProtectedRoute>
-                <PersonasPage />
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path="/personas"
+              element={
+                <ProtectedRoute>
+                  <PersonasPage />
+                </ProtectedRoute>
+              }
+            />
 
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </AppDataProvider>
     </AuthProvider>
   );
 }
